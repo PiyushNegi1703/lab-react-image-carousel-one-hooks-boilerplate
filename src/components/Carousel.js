c4355b930cbbd62da6c0edeafccb6f0ee05a85bd
Piyush Negi <negip7740@gmail.com>
2022-12-28 16:59:12 +0530

    carousel with hooks

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,6 +8,31 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [img,setImg]=useState(0)
+    const back=()=>{
+        if(img>0){
+            setImg(img-1)
+        }
+        else{
+            setImg(2)
+        }
+    }
+    const front =()=>{
+        if(img<2){
+            setImg(img+1)
+        }
+        else{
+            setImg(0)
+        }
+    }
+
+    return(
+        <div id="display">
+            <ArrowBackIosIcon onClick={back} />
+            <img src={images[img].img} alt="hjdoh" />
+            <ArrowForwardIosIcon onClick={front} />
+        </div>
+    )
 }
 
 export default Carousel;
